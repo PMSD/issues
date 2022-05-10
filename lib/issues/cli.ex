b8b896c78636d23381f9f9b1c73568a274a08200
Pauliknows <pamaalouf@gmail.com>
2022-05-10 13:29:31 -0500

    added poison JSON parsing library and parse the response from the HTTPPoison fetch

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -45,6 +45,14 @@ defmodule Issues.CLI do
 
   def process({user, project, _count}) do
     Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
+  end
+
+  def decode_response({:ok, body}), do: body
+   
+  def decode_response({:error, error}) do
+    IO.puts "Error fetching from GitHub: #{error}"
+    System.halt(2)
   end
   
 
